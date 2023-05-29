8c84215854b11573fe34974ac04ea1ba05b5adf8
Dmytro Shevchenko <dmytro.shevchenko@e-hps.com>
2023-05-29 10:35:09 -0400

    Changed plugin name to avoid duplication

--- packages/barcode-scanning/src/index.ts
+++ packages/barcode-scanning/src/index.ts
@@ -2,9 +2,9 @@ import { registerPlugin } from '@capacitor/core';
 
 import type { BarcodeScannerPlugin } from './definitions';
 
-const BarcodeScanner = registerPlugin<BarcodeScannerPlugin>('BarcodeScanner', {
+const BarcodeScannerML = registerPlugin<BarcodeScannerPlugin>('BarcodeScannerML', {
   web: () => import('./web').then(m => new m.BarcodeScannerWeb()),
 });
 
 export * from './definitions';
-export { BarcodeScanner };
+export { BarcodeScannerML };
